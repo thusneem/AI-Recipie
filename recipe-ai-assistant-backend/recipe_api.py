39aa78c4dc1fed244d5d44a88835a4d7e386ae26
thusneem <thusneem@yahoo.com>
2026-01-26 12:32:26 -0500

    updated secrets manager

--- recipe-ai-assistant-backend/recipe_api.py
+++ recipe-ai-assistant-backend/recipe_api.py
@@ -1,13 +1,28 @@
 import requests
-from config import SPOONACULAR_KEY
+import boto3
+import json
+
+def get_spoonacular_key():
+    client = boto3.client("secretsmanager", region_name="us-east-1")
+    secret = client.get_secret_value(
+        SecretId="prod/my-app/api-keys" 
+    )
+    return json.loads(secret["SecretString"])["SPOONACULAR_KEY"]
+
 
 def fetch_recipes(query, diet=""):
+    api_key = get_spoonacular_key()
+
     url = "https://api.spoonacular.com/recipes/complexSearch"
     params = {
-        "apiKey": SPOONACULAR_KEY,
+        "apiKey": api_key,
         "query": query,
         "diet": diet,
         "number": 5,
         "addRecipeInformation": True
     }
-    return requests.get(url, params=params).json()["results"]
+
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    return response.json().get("results", [])
